368ee2f382938f1f0f0142328a2f1fd1945b37dc
Lucas Bracamonte <ing.lucasbracamonte@gmail.com>
2024-11-14 15:39:32 -0300

    primer commit

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# Título de la aplicación
+st.title("Hola desde Streamlit Cloud")
+
+# Botón para mostrar el mensaje
+if st.button("Mostrar mensaje"):
+    st.write("Hola mundo")
